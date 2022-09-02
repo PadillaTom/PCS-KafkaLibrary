e0edb6deb989b3b411f34f35cb4a1ef3eff256a9
Tomas Padilla <padillatomasagustin@gmail.com>
2022-09-02 09:48:21 +0200

    [master] - Starter Project for PRODUCER

---
+++ library-events-producer/src/test/java/com/padillatomas/libraryeventsproducer/LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padillatomas.libraryeventsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

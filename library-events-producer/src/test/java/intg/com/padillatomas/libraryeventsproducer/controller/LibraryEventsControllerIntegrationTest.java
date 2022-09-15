c700ed30aa214860107fe6cbde117fc917c60be9
Tomas Padilla <padillatomasagustin@gmail.com>
2022-09-15 12:24:10 +0200

    tests - Added Integration Test with EmbeddedKafka and Timeout

--- library-events-producer/src/test/java/intg/com/padillatomas/libraryeventsproducer/controller/LibraryEventsControllerIntegrationTest.java
+++ library-events-producer/src/test/java/intg/com/padillatomas/libraryeventsproducer/controller/LibraryEventsControllerIntegrationTest.java
@@ -2,33 +2,85 @@ package com.padillatomas.libraryeventsproducer.controller;
 
 import com.padillatomas.libraryeventsproducer.domain.Book;
 import com.padillatomas.libraryeventsproducer.domain.LibraryEvent;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(
+        topics = {"library-events"},
+        partitions = 3
+)
+@TestPropertySource(
+        properties = {
+                "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+                "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
+        }
+)
 class LibraryEventsControllerIntegrationTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
     @LocalServerPort
     int randomServerPort;
 
+    private Consumer<Integer, String> consumer;
+
+
+    @BeforeEach
+    void setUp()
+    {
+        Map<String, Object> configsForConsumer = new HashMap<>(
+                KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker)
+        );
+
+        consumer = new DefaultKafkaConsumerFactory<Integer, String>(
+                configsForConsumer,
+                new IntegerDeserializer(),
+                new StringDeserializer()
+        ).createConsumer();
+
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        consumer.close();
+    }
+
     /**
      * POST at /api/v1/library-event
-     * TODO: So far we are testing only the Integration of POST, we need to test KAFKA on it's own.
      * */
     @Test
-    void postLibraryEvent() throws URISyntaxException {
+    @Timeout(3)
+    void postLibraryEvent() throws URISyntaxException, InterruptedException {
         // Given:
         Book myBook = Book.builder()
                 .withId(123)
@@ -52,7 +104,18 @@ class LibraryEventsControllerIntegrationTest {
                 testRestTemplate.exchange(myURI, HttpMethod.POST, request, LibraryEvent.class);
 
         // Then:
+
+        // POST
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+        // Message
+        ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+
+        // Thread.sleep(3000); // Will wait 3s before executing the rest of the code. Should be replaced with @Timeout from Junit
+
+        String expectedRecord = "{\"id\":null,\"libraryEventType\":\"NEW\",\"book\":{\"id\":123,\"title\":\"POST Book\",\"author\":\"POST Book\"}}";
+        String singleRecordValue = singleRecord.value();
+        assertEquals(expectedRecord,singleRecordValue);
 
     }
 
